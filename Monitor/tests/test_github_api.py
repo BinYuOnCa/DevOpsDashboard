1729fd602ef0d10490252dfc7fd8327c8c1e1eb9
Your Name <you@example.com>
2020-10-14 20:03:49 -0400

    Rename test case file.

--- Monitor/tests/test_github_api.py
+++ Monitor/tests/test_github_api.py
@@ -2,6 +2,9 @@ import unittest
 from github import github
 
 
+"""
+Set worker director to "/path/to/DevOpsDashboard/Monitor"
+"""
 class GithubTestCases(unittest.IsolatedAsyncioTestCase):
     async def test_fetch_commits(self):
         commits = await github.get_commits('zio', 'zio')
